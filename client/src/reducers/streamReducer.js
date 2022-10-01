8a82e1b1549a0ebfb83037862d1bc66599a17bee
Matthew Henderson <info@matthewahenderson.com>
2022-10-01 16:08:19 -0400

    Added stream reducer

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,16 @@
+import { 
+    FETCH_STREAM, 
+    CREATE_STREAM, 
+    FETCH_STREAMS, 
+    DELETE_STREAM, 
+    EDIT_STREAM 
+} from "../actions/types";
+
+export default (state={}, action) => {
+    switch(action.type) {
+        case FETCH_STREAM: 
+            return { ...state, [action.payload.id]: action.payload };
+        default:
+            return state;
+    }
+};
