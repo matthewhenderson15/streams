42775a508f22a141f4436f8ad14ba2e57a138168
Matthew Henderson <info@matthewahenderson.com>
2022-10-01 11:52:58 -0400

    Added componentDidMount button for getting auth object and then modifying state based on sign in status; created helper function for displaying the correct sign-in or sign-out button

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+    state = { isSignedIn: null };
+    
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '1094184269941-dtbk346nivignnot9542p55p1s2o9jg3.apps.googleusercontent.com',
+                scope: 'email',
+                plugin_name: 'streamy'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    }
+    
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
